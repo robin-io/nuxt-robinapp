28a978ca2a46c969e975a22b34386cf8ac6368a6
elvis-chuks <celvischuks@gmail.com>
2021-11-04 22:48:58 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <RobinChat 
+  :api-key="'NT-CDuBlRKXV....PcGPMBnwHaoYbW'"
+  :user-token="user_token"
+  :user-name="user_name"
+  :users="users"
+  :keys="keys"
+  :page-loader="true"
+  />
+</template>
+
+<script>
+// if the app keeps loading, please confirm you have the correct user_token and api_key
+export default {
+  name: 'Chat',
+  data() {
+    return {
+      user_token: 'JUevyX.....AStMSrXR',
+      user_name: 'Elvis Chuks',
+      users:[],
+      keys:{
+				userToken: 'user_token',
+        profileImage: 'profile_image',
+        userName: 'fullname'
+		  }
+    }
+  },
+  methods: {
+    getUsers() {
+      fetch('https://your-api/endpoint/for/getting/all/users')
+      .then(resp => resp.json())
+      .then(data => {
+        if (!data.error) {
+          this.users = data.users
+          /* 
+          notice how the keys in the response object, align with the values in the keys prop
+          {
+            error : false,
+            users: [
+              {
+                'user_token': 'asdsfa;sds',
+                'profile_image: 'https://some-url.com/photo.png',
+                'fullname': 'Michael Jordan'
+                '
+              },
+              {
+                'user_token': 'asdsfa;sds',
+                'profile_image: 'https://some-url.com/photo.png',
+                'fullname': 'Lebron James'
+                '
+              },
+            ]
+          }
+          */
+        }
+      })
+    }
+  }
+}
+</script>
---
+++ plugins/robinapp.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import RobinChat from 'robin.io-vue'
+import 'robin.io-vue/dist/style.css' //  The component itself does not include any CSS. You'll need to include it separately
+
+Vue.use(RobinChat)
